1a6149da70b55cb0036b8b6d605d389bed0cdbee
Comfortably Numb <masoud@Is-there-anybody-out-there.local>
2023-03-13 21:08:17 +0100

    init

--- laboratoriesMLPR/main.py
+++ laboratoriesMLPR/main.py
@@ -2,5 +2,5 @@ import load
 import plot
 if __name__ == '__main__':
     data = load.Load("Data/iris.csv")
-    plot.histopram(data.samples, data.labels)
+    plot.scatter(data.samples, data.labels)
 
--- laboratoriesMLPR/plot.py
+++ laboratoriesMLPR/plot.py
@@ -1,7 +1,35 @@
 import numpy
 import matplotlib.pyplot as plot
 def scatter(samples, labels):
-    pass
+    class0Mask = labels == 0
+    class1Mask = labels == 1
+    class2Mask = labels == 2
+
+    samplesClass0 = samples[:, class0Mask]
+    samplesClass1 = samples[:, class1Mask]
+    samplesClass2 = samples[:, class2Mask]
+
+    hFea = {
+        0: 'Sepal length',
+        1: 'Sepal width',
+        2: 'Petal length',
+        3: 'Petal width'
+    }
+
+    for i in range(samples.shape[0]):
+        for j in range(samples.shape[0]):
+            if i == j:
+                continue
+            else:
+                plot.scatter(samplesClass0[i, :], samplesClass0[j, :], label="Iris-setosa")
+                plot.scatter(samplesClass1[i, :], samplesClass1[j, :], label="Iris-versicolor")
+                plot.scatter(samplesClass2[i, :], samplesClass2[j, :], label="Iris-virginica")
+                plot.xlabel(hFea[i])
+                plot.ylabel(hFea[j])
+                plot.legend()
+                plot.show()
+
+
 
 def histopram(samples, labels):
     class0Mask = labels == 0
@@ -17,6 +45,6 @@ def histopram(samples, labels):
         plot.hist(samplesClass0[feature,:], density=True, bins=10, alpha = 0.4, label="Iris-setosa")
         plot.hist(samplesClass1[feature,:], density=True, bins=10, alpha = 0.4,label="Iris-versicolor")
         plot.hist(samplesClass2[feature,:], density=True, bins=10, alpha = 0.4,label="Iris-virginica")
-        plot.legend
+        plot.legend()
         plot.show()
-        plot.savefig('hist_%d.png' % feature+1)
+        # plot.savefig('hist_%d.png' % feature+1)
