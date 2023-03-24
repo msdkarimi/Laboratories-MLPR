96ab83dedd829ded42be6bd15d533fe3519a3ae3
Comfortably Numb <masoud@Is-there-anybody-out-there.local>
2023-03-24 15:49:00 +0100

    LDA

--- laboratoriesMLPR/main.py
+++ laboratoriesMLPR/main.py
@@ -1,22 +1,31 @@
 import numpy
-
 import load
 import plot
 import util
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
     try:
         data = load.Load("Data/iris.csv")
         # plot.histopram(data.samples, data.labels)
         # plot.scatter(data.samples, data.labels)
-        pca4 = numpy.load("laboratories result/IRIS_PCA_matrix_m4.npy")
+        # pca4 = numpy.load("laboratories result/IRIS_PCA_matrix_m4.npy")
 
-        mapped, P = util.PCA(data.samples, util.covarinceMartix(data.samples), 4)
-        print(P)
-        print(mapped.shape)
-        plot.scatter(mapped, data.labels)
+        # mapped, P = util.PCA(data.samples, util.covarinceMartix(data.samples), 4)
+        # tst = numpy.dot(P, mapped)
+        # print(tst[:,149]-data.samples[:,149])
+
+        lda2 = numpy.load("laboratories result/IRIS_LDA_matrix_m2.npy")
+        print(lda2)
+        d, r= util.LDA(data.samples, data.labels)
+        print(r)
+
+
+
+
 
 
 
 
     except Exception as e:
-        print(e)
+
+        print(f"exception in main->>> {e}")
--- laboratoriesMLPR/util.py
+++ laboratoriesMLPR/util.py
@@ -1,6 +1,7 @@
 import array
 
 import numpy
+import scipy
 
 def toCol(theArray:numpy.array):
     try:
@@ -64,10 +65,44 @@ def PCA(samples: numpy.array, covarianceMatrix: numpy.array, m: int = 4)->tuple:
     except Exception as e:
         print(e)
 
-def LDA():
+def LDA(samples: numpy.array, labels: numpy.array, m: int = 2):
 
     try:
-        pass
+        sW, sB = getSW_SB(samples, labels)
+        s, U = scipy.linalg.eigh(sB, sW)
+        W = U[:, ::-1][:, 0:m]
 
+        mapped = numpy.dot(W.T, samples)
+        return mapped, W
+
+    except Exception as e:
+        print(e)
+
+
+def getSW_SB(dataSet, labels):
+    try:
+        sW = 0
+        for i in range(len(set(labels))):
+            eachClass = dataSet[:, labels == i]
+            nEachClass = eachClass.shape[1]
+            swCTemp = covarinceMartix(eachClass)
+            swCTemp = swCTemp * nEachClass
+            sW = sW + swCTemp
+
+        sW = sW / dataSet.shape[1]
+
+        sB = 0
+        for i in range(len(set(labels))):
+            mu = mean(dataSet)
+            eachClass = dataSet[:, labels == i]
+            nEachClass = eachClass.shape[1]
+            muC = mean(eachClass)
+            sBtemp = nEachClass * numpy.dot((muC - mu), (muC - mu).T)
+
+            sB = sB + sBtemp
+
+        sB = sB / dataSet.shape[1]
+
+        return sW, sB
     except Exception as e:
         print(e)
